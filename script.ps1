51b323b1f3cb5243874c09d16da380f96ba8d0d4
Maximilián Harazin <50361989+harazin@users.noreply.github.com>
2022-07-27 12:19:01 +0100

    Update script.ps1 to include main packages

--- script.ps1
+++ script.ps1
@@ -1,12 +1,24 @@
 # This script can be run remotely by running the below commands:
-# $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/harazin/win-sandbox/main/script.ps1
-# Invoke-Expression $($ScriptFromGithHub.Content)
+# Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/harazin/win-sandbox/main/script.ps1)
 
 # Script Start
 
 Write-Host " "
-Write-Host "Here are the top 10 CPU consuming processes right now"
+Write-Host "Current processes:"
 Write-Host " "
+
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
+
+Set-ExecutionPolicy Unrestricted -Force
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install nodejs.install vscode python3 googlechrome -Confirm
+
+Write-Host " "
+Write-Host "Script completed succesfully"
+Write-Host " "
 
 # Script End
